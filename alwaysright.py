b98a6878cfc79fb9b31aa2a05b73a686384666fe
duckycoder0630 <129128252+duckycoder0630@users.noreply.github.com>
2026-05-16 00:52:23 +0000

    please generate new values

--- alwaysright.py
+++ alwaysright.py
@@ -2,14 +2,17 @@ import random
 
 async def main():
     while True:
-        hi = await input("What fact would you like to check?: ")
+        await input("What fact would you like to check?: ")
+
         a = random.randint(1, 100)
         b = random.randint(1, 100)
         c = random.randint(1, 100)
         d = random.randint(1, 100)
         e = random.randint(1, 100)
+
         print("That is:", max(a, b, c, d, e), "% right.")
-        if (await input("Do you want to convert something else? (y/n)\n")).strip().lower() == "n":
+        answer = (await input("Do you want to convert something else? (y/n)\n")).strip().lower()
+        if answer == "n":
             break
 
 globals()["main"] = main
